ca9367f945cbba1c13160a4aed8290e232c678d6
xiongzhengyao <xiongzhengyao@xiaomi.com>
2025-04-27 20:30:03 +0800

    update

---
+++ headfiles/sample.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+enum {
+    SAMPLE_SIZE = 1,
+    SAMPLE_RATE = 44100,
+    SAMPLE_DURATION = 1,
+    SAMPLE_CHANNELS = 1,
+};
+
+int main(int argc, char *argv[])
+{
+    printf("Hello, World!\n");
+    return 0;
+}
